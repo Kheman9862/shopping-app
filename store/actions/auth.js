f2e8513f7767faafea53ec468b07e0c303a6529b
Kheman9862 <55815813+Kheman9862@users.noreply.github.com>
2020-08-23 22:22:57 -0400

    Update auth.js

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -17,7 +17,7 @@ export const authenticate = (userId, token, expiryTime) => {
 export const signup = (email, password) => {
   return async (dispatch) => {
     const response = await fetch(
-      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAgPrnAzAR-1Nq-XCzdRQvBkUuVQOv44xA",
+      "xyz"
       {
         method: "POST",
         headers: {
@@ -59,7 +59,7 @@ export const signup = (email, password) => {
 export const login = (email, password) => {
   return async (dispatch) => {
     const response = await fetch(
-      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAgPrnAzAR-1Nq-XCzdRQvBkUuVQOv44xA",
+      "xyz"
 
       {
         method: "POST",
